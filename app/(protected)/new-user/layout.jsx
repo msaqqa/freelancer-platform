22bbd02de4e20be480e2c2db93c1e01995054323
masaqqa <ma.saqqa@gmail.com>
2025-06-16 15:22:31 +0300

    run permssion

--- app/(protected)/new-user/layout.jsx
+++ app/(protected)/new-user/layout.jsx
@@ -6,26 +6,26 @@ import { useAuth } from '@/hooks/auth/use-auth';
 import { ScreenLoader } from '@/components/common/screen-loader';
 
 function NewUserlayout({ children }) {
-  // const { data: user, isLoading, isError } = useAuth();
-  // const router = useRouter();
+  const { data: user, isLoading, isError } = useAuth();
+  const router = useRouter();
 
-  // useEffect(() => {
-  //   if (isLoading) return;
-  //   if (!user || isError) {
-  //     router.push('/signin');
-  //   }
-  //   if (user?.type && !user?.save_data) {
-  //     router.push('/new-user/required-data');
-  //   } else if (!user?.type && !user?.save_data) {
-  //     router.push('/new-user/account-type');
-  //   } else {
-  //     router.push('/404');
-  //   }
-  // }, [isLoading, user, router]);
+  useEffect(() => {
+    if (isLoading) return;
+    if (!user || isError) {
+      router.push('/signin');
+    }
+    if (user?.type && !user?.save_data) {
+      router.push('/new-user/required-data');
+    } else if (!user?.type && !user?.save_data) {
+      router.push('/new-user/account-type');
+    } else {
+      router.push('/404');
+    }
+  }, [isLoading, user, router]);
 
-  // if (isLoading) {
-  //   return <ScreenLoader />;
-  // }
+  if (isLoading) {
+    return <ScreenLoader />;
+  }
 
   return children;
 }
